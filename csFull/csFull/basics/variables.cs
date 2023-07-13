5033937b6767ebaeb6d8b91cac7e551939a97e9d
MostafaMohamedTaha <MostafaMohamedTaha.GitHup@gmail.com>
2023-07-13 07:35:48 +0200

    till Array 3h

---
+++ csFull/csFull/basics/variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csFull.basics
+{
+    internal class variables
+    {
+        //int a=0;
+        //string s = "mo";
+        //var v = 1;
+        //var vv = "mo2"; only with main 
+        //dynamic d = 10;
+    }
+}
---
+++ csFull/csFull/cs1Basics.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using csFull.basics;
+
+namespace csFull
+{
+    internal class cs1Basics
+    {
+        ///////////////////////////////////
+        //static void Main(string[] args)
+        //{
+        //int a=0;
+        //string s = "mo";
+        //var v = 1;
+        //var vv = "mo2"; only with main 
+        //dynamic d = 10;
+        //}
+        /////////////////////////////////////
+        // static void Main(string[] args)
+        //{
+        //    bool a=true;
+        //    int b=0;    
+        //    int c=0;
+        //    a = b == c;
+        //    Console.WriteLine(a);
+        //}
+        /// <summary>
+        /// ////////////////////////
+        /// </summary>
+        /// <param name="args"></param>
+        //static void Main(string[] args)
+        //{
+        //    int x=1,y=2;
+        //    bool a = x >= y;
+        //    Console.WriteLine(a);
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    //Xor 
+        //    Console.WriteLine(false ^ false); //false
+        //    Console.WriteLine(false ^ true);    //true
+        //    Console.WriteLine(true ^ false); //true
+        //    Console.WriteLine(true ^ true); //false
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(true&true); //true //short cuircut
+        //    Console.WriteLine(true&false); //false 
+        //    Console.WriteLine(false&true); //false
+        //    Console.WriteLine(false & false); //false
+        //    Console.WriteLine(false | false); //false //short cuircut
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    //arr
+        //    var a = new string[] { "a","b","c","d","f" };
+        //    int[] b = new int[] { 1, 2, 3, 4, 5 };
+        //    int[] c = { 1, 2, 3, 4, 5 };
+        //    //2darr,3darr[,,]
+        //    int[,] d = { { 1, 2, 3, 4, 5 },{ 1, 2, 3, 4, 5 },{ 1, 2, 3, 4, 5 } };
+        //    //jagged array
+        //    var f = new int[][]
+        //    {
+        //        new int[] { 1, 2, 3, 4, 5 },
+        //        new int[] { 1, 2, 3, 4, 5 },
+        //        new int[] { 1, 2, 3, 4, 5 },
+        //    };
+        //    var e = a[..2]; //c# 8=>
+        //    Console.WriteLine(e);
+
+        //}
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
