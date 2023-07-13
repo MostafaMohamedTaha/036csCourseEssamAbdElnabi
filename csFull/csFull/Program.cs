b91bb9d4eecb9647e69dc7ed54f028a253faf313
MostafaMohamedTaha <MostafaMohamedTaha.GitHup@gmail.com>
2023-07-13 05:47:33 +0200

    start

---
+++ csFull/csFull/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csFull
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
